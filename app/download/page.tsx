15604135e648baccdadbde6989140b961703ed80
elz3eem001 <elz3eem001@gmail.com>
2026-06-02 00:15:10 +0300

    Add files via upload

--- app/download/page.tsx
+++ app/download/page.tsx
@@ -3,7 +3,7 @@ import { Footer } from "@/components/footer"
 import { Button } from "@/components/ui/button"
 import { Download, Monitor, CheckCircle2 } from "lucide-react"
 
-const DOWNLOAD_URL = "https://release-assets.githubusercontent.com/github-production-release-asset/1253711537/5d8498fc-2d63-45d7-8dbf-7fd3dadb9c17?sp=r&sv=2018-11-09&sr=b&spr=https&se=2026-06-01T19%3A47%3A33Z&rscd=attachment%3B+filename%3DX-WorkFlow-Setup-1.0.0.exe&rsct=application%2Foctet-stream&skoid=96c2d410-5711-43a1-aedd-ab1947aa7ab0&sktid=398a6654-997b-47e9-b12b-9515b896b4de&skt=2026-06-01T18%3A47%3A03Z&ske=2026-06-01T19%3A47%3A33Z&sks=b&skv=2018-11-09&sig=sQ3xVbBXp3ApV1uQM0cK0gv8hYEPvB7doNP5XFs3y%2F8%3D&jwt=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmVsZWFzZS1hc3NldHMuZ2l0aHVidXNlcmNvbnRlbnQuY29tIiwia2V5Ijoia2V5MSIsImV4cCI6MTc4MDM0MjAzMCwibmJmIjoxNzgwMzQwMjMwLCJwYXRoIjoicmVsZWFzZWFzc2V0cHJvZHVjdGlvbi5ibG9iLmNvcmUud2luZG93cy5uZXQifQ.FpKPoTCi88JxCTQJZig3dpy3Wei_OhuvxJKDxTN6FF8&response-content-disposition=attachment%3B%20filename%3DX-WorkFlow-Setup-1.0.0.exe&response-content-type=application%2Foctet-stream"
+const DOWNLOAD_URL = "https://github.com/MrX-619/Mr-x/releases/download/v1.0.0/X-WorkFlow-Setup-1.0.0.exe"
 
 const requirements = [
   "نظام التشغيل: Windows 10 أو أحدث",
